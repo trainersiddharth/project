f9975d6fee0caae4fa261f45a5d2358355cd3a6a
Siddharth Jain <trainer.siddharth@gmail.com>
2021-05-16 00:00:15 +0530

    Data changed

--- application/models/Dashboard_Model.php
+++ application/models/Dashboard_Model.php
@@ -29,9 +29,16 @@
             return $query->num_rows();
         }
 
+        //select y.name as "Centre", count(x.id) as "students" , sum(x.no_of_courses) as "Courses" , sum(x.duration_of_courses) as "Duration" from students x join centres y on x.centre_code=y.id  group by x.centre_code,y.name 
         public function instituteBatchCount(){
             
             $query=$this->db->query('select y.name as "Centre", sum(if(x.batch_code=1,1,0)) as "Batch-1" , sum(if(x.batch_code=2,1,0)) as "Batch-2" , sum(if(x.batch_code=3,1,0)) as "Batch-3" ,y.poc_name, y.trainer_name ,y.email from students x join centres y on x.centre_code=y.id  group by x.centre_code,y.name');
+            return $query->result_array();
+        }
+
+        public function skillsbuildCountCentreWise(){
+            
+            $query=$this->db->query('select y.name as "centre", count(x.id) as "students" , sum(x.no_of_courses) as "courses" , sum(x.duration_of_courses) as "duration" from students x join centres y on x.centre_code=y.id  group by x.centre_code,y.name');
             return $query->result_array();
         }
 
